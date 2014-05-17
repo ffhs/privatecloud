b92fd0cfa63cafac9028678e83912bf83e199071
Thierry Baumann <thierry@swissmademarketing.com>
2014-05-17 16:23:43 +0200

    gen weg

--- PrivateCloudFFHS/gen/ch/ffhs/privatecloudffhs/R.java
+++ PrivateCloudFFHS/gen/ch/ffhs/privatecloudffhs/R.java
@@ -1205,15 +1205,18 @@ containing a value of this type.
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+
     
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
+
         
 
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
+
     
  API 11 theme customizations can go here. 
 
