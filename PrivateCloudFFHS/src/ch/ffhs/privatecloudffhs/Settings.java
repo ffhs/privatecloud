2ef0eb8707442974c3354360f751f5eac9f2f0c3
P45Q <P45q@P45Q.net>
2014-05-23 18:39:59 +0200

    ijoaisd

--- PrivateCloudFFHS/src/ch/ffhs/privatecloudffhs/Settings.java
+++ PrivateCloudFFHS/src/ch/ffhs/privatecloudffhs/Settings.java
@@ -13,7 +13,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class Settings extends Activity {
@@ -44,11 +46,26 @@ public class Settings extends Activity {
         adapter	= new ServerListAdapter(contex);
         listView.setAdapter(adapter);
 		
-		
+        listView.setClickable(true);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+          @Override
+          public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+            Object o = listView.getItemAtPosition(position);
+            Log.d("jada","clicked");
+            //Intent activityserver = new Intent();
+			//activityserver.putExtra("serverid", db.);
+			//startActivity(activityserver);
+          }
+        });
 //		SharedPreferences settings = getSharedPreferences(R.string.perfname,MODE_PRIVATE);
  //       String hostname = settings.getString(R.string.perfs_hostname);
         
 	}
+
+
+	
+
     public void onButtonClicked(View v){
     	switch(v.getId()) {
     		case R.id.Settings_Button_cancel:
@@ -57,7 +74,7 @@ public class Settings extends Activity {
     		case R.id.Settings_Button_Add:
     			int serverid = 0;
     			Intent activityserver = new Intent(this,ActivityServer.class);
-    			//activityserver.putExtra("serverid", serverid);
+    			activityserver.putExtra("serverid", serverid);
     			startActivity(activityserver);
     		break;
     		
